3ed41f92b8f8f536f023984fcad8511dfe1ead5b
Fredrik <fresh@superburken.se>
2023-08-08 20:31:38 +0200

    init

---
+++ cpu.py
@@ -0,0 +1,63 @@
+"""
+MIT License
+
+Copyright (c) Fredrik B
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE
+"""
+
+import func
+import time
+import math
+import multiprocessing
+from multiprocessing import Value
+
+NUMBER = 10000000
+
+if __name__ == "__main__":
+    cpu_count = func.get_cpu_count()
+    ranges = func.get_ranges(NUMBER, cpu_count)
+    
+    total_prime_count = 0
+    procs = []
+    values = []
+
+    start_time = time.time()
+
+    for x in ranges:
+        value = Value('i', 0)
+        proc = multiprocessing.Process(target=func.crunch_range, args=(value, x.f, x.t), daemon=False)
+
+        procs.append(proc)
+        values.append(value)
+
+        proc.start()
+
+    print("crunching primes from number " + str(NUMBER) + " using " + str(cpu_count) + " cores")
+
+    for x in procs:
+        x.join()
+
+    for x in values:
+        total_prime_count += x.value
+
+    stop_time = time.time()
+    seconds = math.floor(stop_time - start_time)
+
+    print("Found " + str(total_prime_count) + " primes in " + str(seconds) + " seconds")
---
+++ func.py
@@ -0,0 +1,84 @@
+"""
+MIT License
+
+Copyright (c) Fredrik B
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE
+"""
+
+import math
+import multiprocessing
+
+class Range:
+    f:int = 0
+    t:int = 0
+
+    def __init__(self, f: int, t: int) -> None:
+        self.f = f
+        self.t = t
+
+def get_cpu_count() -> int:
+    return multiprocessing.cpu_count()
+
+def crunch_range(value, f: int, t: int):
+    prime_count = 0
+
+    for n in range(f, t + 1):
+        if is_prime(n):
+            prime_count += 1
+
+    value.value = prime_count
+
+def get_ranges(number: int, count: int) -> [] :
+    ranges = []
+    size = math.floor(number / count)
+    remainder = number % count
+
+    f = 1
+    t = size + remainder
+
+    for i in range(count):
+        r = Range(f, t)
+        ranges.append(r)
+
+        f = t + 1
+        t = f + size
+
+        if t > number:
+            t = number
+        
+    return ranges
+
+def is_prime(number: int) -> bool:
+    if number < 2:
+        return False
+    
+    if number == 2 or number == 3:
+        return True
+    
+    if number % 2 == 0 or number % 3 == 0:
+        return False
+    
+    end = math.ceil(math.sqrt(number))
+
+    for x in range(5, end + 1):
+        if number % x == 0:
+            return False
+        
+    return True
